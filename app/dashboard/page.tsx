582dc7e431a883386d07c608af09f9d8afcf449f
Lucas E Neiva <neiva.lucas13@gmail.com>
2026-01-02 14:47:10 -0300

    feat: add attributes page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -49,6 +49,26 @@ export default async function AdminDashboard() {
                         </Link>
                     </div>
 
+                    <div className="column md:grid-cols-3 gap-6">
+                    {/* Card de Produtos */}
+                    <div className="p-6 border rounded-xl bg-white hover:shadow-md transition cursor-pointer flex flex-col">
+                        <h2 className="text-lg font-bold text-gray-900">
+                            Atributos Cadastrados
+                        </h2>
+
+                        <p className="text-sm text-gray-500 mt-2">
+                            Gerenciar os atributos para produtos
+                        </p>
+
+                        <Link
+                            href="/dashboard/attributes"
+                            className="mt-4 inline-block bg-black text-white px-5 py-2.5 rounded-lg text-sm font-medium hover:bg-gray-800 w-fit"
+                        >
+                            Ver Produtos
+                        </Link>
+                    </div>
+
+                </div>
                 </div>
             </div>
         </div>
