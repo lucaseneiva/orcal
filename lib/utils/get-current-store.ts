c5500389c3783d26640b65f0f059a56c96f052ff
Lucas E Neiva <neiva.lucas13@gmail.com>
2026-01-03 17:06:21 -0300

    feat: add fetch forwarded-host

--- lib/utils/get-current-store.ts
+++ lib/utils/get-current-store.ts
@@ -4,10 +4,15 @@ import { getStoreRepository } from '@/lib/services/store-service';
 
 export const getCurrentStore = cache(async () => {
   const headerStack = await headers();
-  const host = headerStack.get('host');
+  
+  const rawHost = headerStack.get('x-forwarded-host') || headerStack.get('host');
+  
+  const host = rawHost?.split(':')[0]; 
+
+  console.log('[DEBUG] Host final processado:', host);
 
   const storeRepository = await getStoreRepository();
-  const store = storeRepository.findByDomain(host);
+  const store = await storeRepository.findByDomain(host || null);
 
   if (!store) return null;
   return store;
