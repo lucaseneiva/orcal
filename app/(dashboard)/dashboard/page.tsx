fc6066334b6be3e54e296c169bdee14b75127233
Lucas E. Neiva <neiva.lucas13@gmail.com>
2026-01-14 13:05:52 -0300

    chore: visual tweaks dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,7 @@
 import { createClient } from '@/lib/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import { getCurrentStore } from '@/lib/utils/get-current-store'
-import Link from 'next/link'
-
+import { DashboardCards } from './components/DashboardCards'
 export default async function AdminDashboard() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
@@ -15,53 +14,7 @@ export default async function AdminDashboard() {
     <div className="p-10">
       <div className="max-w-2xl mx-auto px-4">
         <div className="grid md:grid-cols-2 gap-6">
-          {/* Card de Produtos */}
-          <div className="p-6 border rounded-xl bg-white hover:shadow-md transition cursor-pointer flex flex-col">
-            <h2 className="text-lg font-bold text-gray-900">
-              Catálogo de Produtos
-            </h2>
-            <p className="text-sm text-gray-500 mt-2">
-              Gerenciar produtos
-            </p>
-            <Link
-              href="/dashboard/products"
-              className="mt-4 inline-block bg-black text-white px-5 py-2.5 rounded-lg text-sm font-medium hover:bg-gray-800 w-fit"
-            >
-              Ver Produtos
-            </Link>
-          </div>
-
-          {/* Card de Atributos */}
-          <div className="p-6 border rounded-xl bg-white hover:shadow-md transition cursor-pointer flex flex-col">
-            <h2 className="text-lg font-bold text-gray-900">
-              Atributos Cadastrados
-            </h2>
-            <p className="text-sm text-gray-500 mt-2">
-              Gerenciar os atributos para produtos
-            </p>
-            <Link
-              href="/dashboard/attributes"
-              className="mt-4 inline-block bg-black text-white px-5 py-2.5 rounded-lg text-sm font-medium hover:bg-gray-800 w-fit"
-            >
-              Ver Atributos
-            </Link>
-          </div>
-
-          {/* Card de Atributos */}
-          <div className="p-6 border rounded-xl bg-white hover:shadow-md transition cursor-pointer flex flex-col">
-            <h2 className="text-lg font-bold text-gray-900">
-              Pedidos de Orçamento
-            </h2>
-            <p className="text-sm text-gray-500 mt-2">
-              Gerenciar os seus pedidos de orçamento
-            </p>
-            <Link
-              href="/dashboard/quote-requests"
-              className="mt-4 inline-block bg-black text-white px-5 py-2.5 rounded-lg text-sm font-medium hover:bg-gray-800 w-fit"
-            >
-              Ver Pedidos
-            </Link>
-          </div>
+          <DashboardCards primaryColor={store!.primary_color}></DashboardCards>
         </div>
       </div>
     </div>
