d26fb9e565a610095382f79cdfdeb47ac7d581c0
Lucas E. Neiva <neiva.lucas13@gmail.com>
2026-01-19 14:43:20 -0300

    refactor: edit and create product page

--- app/(dashboard)/dashboard/products/[id]/edit/page.tsx
+++ app/(dashboard)/dashboard/products/[id]/edit/page.tsx
@@ -1,57 +1,77 @@
-import { getProductById } from '@/lib/data/queries/products'
-import { getAttributesByStoreId } from '@/lib/data/queries/attributes'
-import { getCurrentStore } from '@/lib/utils/get-current-store'
 import { notFound, redirect } from 'next/navigation'
-import { ProductForm } from '../../form'
 import { createClient } from '@/lib/utils/supabase/server'
-// Import the interface from the form file
-import { ProductWithOptions } from '@/lib/types/types'
+import { getCurrentStore } from '@/lib/utils/get-current-store'
+import { DashboardPageHeader } from '../../../components/dashboard-page-header'
+import { ProductForm } from '../../form'
+import { ProductOption, ProductWithOptions } from '@/lib/types/types'
 
 interface PageProps {
   params: Promise<{ id: string }>
 }
 
 export default async function EditProductPage({ params }: PageProps) {
-  // 1. Await params
   const { id } = await params
-  
-  // 2. Get Store and Auth context
+
   const store = await getCurrentStore()
   if (!store) redirect('/dashboard')
 
   const supabase = await createClient()
 
-  // 3. Fetch Product data
-  
-  const productData = await getProductById(id)
+  const productQuery = supabase
+    .from('products')
+    .select(`
+      *,
+      products_options (
+        options (
+          id, name, description,
+          attributes ( id, name, slug )
+        )
+      )
+    `)
+    .eq('id', id)
+    .single()
 
-  // 4. Handle 404
-  if (!productData) return notFound()
+  const attributesQuery = supabase
+    .from('attributes')
+    .select('*, options(*)')
+    .eq('store_id', store.id)
 
-  // 5. Fetch Attributes for the sidebar configuration
+  const [productRes, attributesRes] = await Promise.all([productQuery, attributesQuery])
 
-  const allAttributes = await getAttributesByStoreId(store.id)
+  if (productRes.error || !productRes.data) return notFound()
 
-  //6. Format and Cast the product
-  const formattedProduct = {
-    ...productData,
-    options: productData.options || []
-  } as unknown as ProductWithOptions
+  const rawProduct = productRes.data
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const flattenedOptions: ProductOption[] = rawProduct.products_options.map((po: any) => ({
+    option_id: po.options.id,
+    option_name: po.options.name,
+    option_description: po.options.description,
+    attribute_id: po.options.attributes?.id,
+    attribute_name: po.options.attributes?.name,
+    attribute_slug: po.options.attributes?.slug,
+  }))
+
+  const product: ProductWithOptions = {
+    ...rawProduct,
+    options: flattenedOptions
+  }
 
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
-      <div className="max-w-4xl mx-auto mb-6">
-        <h1 className="text-2xl font-bold text-gray-900">Editar Produto</h1>
-        <p className="text-sm text-gray-500">
-          Atualize as informações e atributos do seu produto.
-        </p>
-      </div>
+      <div className="max-w-4xl mx-auto">
+        <DashboardPageHeader
+          title="Editar Produto"
+          subtitle="Atualize as informações e atributos do seu produto."
+          backHref="/dashboard/products"
+        />
 
-      <ProductForm 
-        product={formattedProduct} 
-        allAttributes={allAttributes}
-        primaryColor={store.primary_color}
-      />
+        <ProductForm
+          product={product}
+          allAttributes={attributesRes.data || []}
+          primaryColor={store.primary_color}
+        />
+      </div>
     </div>
   )
 }
--- app/(dashboard)/dashboard/products/new/page.tsx
+++ app/(dashboard)/dashboard/products/new/page.tsx
@@ -1,19 +1,33 @@
 import { ProductForm } from '../form'
-import { getAttributesByStoreId } from '@/lib/data/queries/attributes'
 import { getCurrentStore } from '@/lib/utils/get-current-store'
+import { DashboardPageHeader } from '../../components/dashboard-page-header'
+import { createClient } from '@/lib/utils/supabase/server'
 
 export default async function NewProductPage() {
   const store = await getCurrentStore()
   if (!store) return null
 
-  const allAttributes = await getAttributesByStoreId(store.id)
+  const supabase = await createClient()
+
+  const { data } = await supabase
+    .from('attributes')
+    .select(`
+        *,
+        options (
+          *
+        )
+      `)
+    .eq('store_id', store.id)
+
+  const allAttributes = data || []
 
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
-      <div className="max-w-4xl mx-auto mb-6">
-        <h1 className="text-2xl font-bold text-gray-900">Novo Produto</h1>
-      </div>
-      <ProductForm allAttributes={allAttributes} primaryColor={store.primary_color}/>
+      <DashboardPageHeader
+        title="Novo Produto"
+        backHref="/dashboard"
+      />
+      <ProductForm allAttributes={allAttributes} primaryColor={store.primary_color} />
     </div>
   )
 } 
